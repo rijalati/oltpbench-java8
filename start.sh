b75ab60ecd274cc0cbf70588544fffe7bb759cec
Ritchie J Latimore <ritchie.latimore@bluemedora.com>
2015-11-24 22:32:30 +0000

    script now runs docker in detached mode

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ TEMPLATE=$1
 BENCH=${TEMPLATE##*-}
 BENCH=${BENCH%.xml}
 
-docker run oltpbench-java8 -b ${BENCH} -c /oltpbench/templates/${TEMPLATE} \
+docker run -d oltpbench-java8 -b ${BENCH} -c /oltpbench/templates/${TEMPLATE} \
        --clear true --create --true --execute true --load true
 
 exit 0
